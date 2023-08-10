d051612fa191c5f05d32f79acaa6e8b60582e3e6
vishank <vishankaggarwal1@gmail.com>
2023-08-10 21:24:22 +0530

    inta

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Header } from "./Header";
 import { Heroheader } from "./Heroheader";
@@ -14,8 +11,6 @@ import { Twolast } from "./Twolast";
 import { Last } from "./Last";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header></Header>
